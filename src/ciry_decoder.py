603c11196e3e122522b1bfb694bd771cb347a65e
Milllord <89139957+Milllord@users.noreply.github.com>
2022-03-29 15:12:23 +0300

    Вывод
    1) Приглашение ко вводу города

---
+++ src/ciry_decoder.py
@@ -0,0 +1,4 @@
+
+
+url = f"http://api.openweathermap.org/geo/1.0/direct?q=\
+{city_name},{state_code},{country_code}&limit={limit}&appid={API_key}"
--- src/window.py
+++ src/window.py
@@ -1,4 +1,6 @@
+from cProfile import label
 from tkinter import *
+from turtle import bgcolor
 
 # Ширина экрана
 SCREEN_WIDTH = 1000
@@ -33,7 +35,8 @@ class Window:
         # y = WINDOW_Y
         self.window.geometry(f"{SCREEN_WIDTH}x{SCREEN_HIGHT}+{WINDOW_X}+{WINDOW_Y}")
         self.window["bg"] = "gray22"
-
+        self.label = Label(text="Введите город:", fg="white", bg="gray22", font="Arial 14")
+        self.label.pack()
     def main_loop(self):
         """
         Главная функция обработки событий
