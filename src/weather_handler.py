5032c69d76d2b1a10438558588d8630a1bba0a94
Milllord <89139957+Milllord@users.noreply.github.com>
2022-04-04 23:12:00 +0300

    1) Разобрался с многопоточностью

--- src/weather_handler.py
+++ src/weather_handler.py
@@ -1,26 +1,6 @@
-import multiprocessing
+from threading import *
 import open_weather_map
 
-
-def processed(lon, lat, procs, calc):
-    # procs - количество ядер
-    # calc - количество операций на ядро
-    
-    processes = []
-    
-    # делим вычисления на количество ядер
-    for proc in range(procs):
-        p = multiprocessing.Process(target=open_weather_map.get_open_weather_map_weather(lon, lat), 
-        args=(calc, proc))
-        processes.append(p)
-        p.start()
-    
-    # Ждем, пока все ядра 
-    # завершат свою работу.
-    for p in processes:
-        p.join()
-
 def get_weather(lon, lat):
-    n_proc = multiprocessing.cpu_count()
-    processed(lon, lat, n_proc, calc=1)
-    print("Завершено успешно")
+    thread1 = Thread(target=open_weather_map.get_open_weather_map_weather, args=(lon, lat))
+    # thread2 = Thread(te)
